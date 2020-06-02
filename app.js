6ff8758b4022ad6125de7e7c341cbc30dfcfff24
christianbarreraduarte <christian.barreraucuenca.edu.ec>
2020-06-01 19:05:16 -0500

    version 2

--- app.js
+++ app.js
@@ -3,36 +3,52 @@
 
 angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', function ($scope) {
-  $scope.comidasDia = "pan,cafe";
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject=['$scope', '$filter'];
+function LunchCheckController($scope, $filter) {
+  $scope.comidasDia = "rice, meat";
   $scope.mensaje = "";
   $scope.colorMensaje="";
   $scope.setMensaje = function(){
     var nComidas = contarCadenasSeparadasPorComa($scope.comidasDia);
-    determinarSiSonDemasiadas(nComidas);
+    var respuestas = determinarSiSonDemasiadas(nComidas);
+    $scope.colorMensaje = respuestas[0];
+    $scope.mensaje = respuestas[1];
   };
 
-  function contarCadenasSeparadasPorComa(string){
+  function contarCadenasSeparadasPorComa(string){ //count no empties strings separate for , 
     var tamanioVector = 0;
     var vector = string.split(",")
-    if (string.length>"")
+    var n = 0;
+    var cadena = "";
+    if (string.length>""){
       tamanioVector = vector.length;
-    return tamanioVector;
+      for (var i=0;i<tamanioVector;i++){
+        cadena = vector[i];
+        if (!!cadena)
+          n+=1;
+      }
+    }
+    return n;
   };
-  function determinarSiSonDemasiadas(numeroDeComidas){
-    $scope.colorMensaje="black";
+  function determinarSiSonDemasiadas(numeroDeComidas){ //determinate if too much items
+    var respuestas = new Array();
+    respuestas[0]="black";
     if (numeroDeComidas==0){
-      $scope.mensaje="Ingrese los datos primero";
+      respuestas[1]="Please enter data first";
+      respuestas[0]="red";
     }
     else if (numeroDeComidas>0 && numeroDeComidas<=3){
-      $scope.mensaje="DISFRUTA";
-      $scope.colorMensaje="green";
+      respuestas[1]="Enjoy!";
+      respuestas[0]="green";
     }
     else if (numeroDeComidas>3){
-      $scope.mensaje="Demasiado!!";
-      $scope.colorMensaje="red";
+      respuestas[1]="Too much!!";
+      respuestas[0]="green";
     }
+    return respuestas;
   }
-});
+}
 
 })();
